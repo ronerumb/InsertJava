d59ed470fd0a18409fca0c25655326980cf77bbe
Roner <ronerumb@gmail.com>
2022-12-28 11:48:22 -0300

    projeto completo

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,38 +1,53 @@
 package aplication;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
+import db.DBException;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
-		
+		PreparedStatement ps = null;
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
 		try {
 			conn = DB.getConnection();
-			st = conn.createStatement();
-			rs = st.executeQuery("Select * from department");
-			
-			while(rs.next()) {
-				System.out.println(rs.getInt("Id")+","+rs.getString("Name"));
+			ps = conn.prepareStatement("Insert into seller " + "(Name,Email,BirthDate,BaseSalary,DepartmentId)"
+					+ "values" + " (?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, "Carlos");
+			ps.setString(2, "carlos@gmail.com");
+			ps.setDate(3, new java.sql.Date(sdf.parse("12/02/1999").getTime()));
+			ps.setDouble(4, 3000);
+			ps.setInt(5, 4);
+			int linhas = ps.executeUpdate();
+			if (linhas > 0) {
+
+				ResultSet rs = ps.getGeneratedKeys();
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Id incluido" + id);
+				}
+
+			} else {
+				System.out.println("Nenhuma linha alterada");
 			}
-			
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
+
+		} catch (SQLException e) {
+			throw new DBException(e.getMessage());
+		} catch (ParseException e) {
+			throw new DBException(e.getMessage());
+		} finally {
+			DB.CloseStatement(ps);
 			DB.CloseConnection();
-			DB.CloseStatement(st);
-			DB.CloseResultset(rs);
 		}
 
 	}
-
 }
